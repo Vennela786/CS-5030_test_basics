f676e271a87e793ec58e6fc62697bf62f9235960
Vennela786 <112149392+Vennela786@users.noreply.github.com>
2022-11-07 19:31:17 -0600

    Added remaining testcases

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -19,7 +19,30 @@ describe('todo test suite', () => {
         expect(todo_service.get_todos().todo.length).toEqual(3);
     });
 
+    /*
+        deleteing the todo from the currentTodo list and comparing the length of old todo list with the new length
+    */
+        test("delete_todos" ,()=>{
+            const id = 1;
+            const lengthBefore = todo_service.todos.todo.length;
+            todo_service.delete_todo(id);
+            expect(todo_service.todos.todo.length ).toEqual(lengthBefore -1 );
+        })
+        
     
+        /*
+            updating the todo list data at index 1 with the todo object. 
+        */
+        test("update Todo",() => {
+            const id = 1;
+            const todo = {
+                "title": "T1",
+                "description": "D1",
+                "done": true
+            };
+            todo_service.update_todo(id,todo);
+            expect(todo_service.todos.todo[id]).toEqual(todo);
+        })   
 
     // Write all your test cases here that corresponds to software requirements
 
